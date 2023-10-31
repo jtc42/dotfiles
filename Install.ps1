2ac0e739b72239f210a51c3d67aebae14afb7898
Joel T. Collins <joel.collins@ukkidney.org>
2023-10-31 16:20:35 +0000

    Expand aliases

--- Install.ps1
+++ Install.ps1
@@ -1,20 +1,20 @@
-echo "Configuring execution policy..."
+Write-Output "Configuring execution policy..."
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 $profileDestination = $profile ? $profile : "~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 
-echo "Linking profile to ${profileDestination}..."
+Write-Output "Linking profile to ${profileDestination}..."
 New-Item -Path $profileDestination -ItemType SymbolicLink -Value (Get-Item ".\Microsoft.PowerShell_profile.ps1").FullName
 
 # Install packages required for profile
 
 # Install scoop
 
-if ((Get-Command "scoop" -ErrorAction SilentlyContinue) -eq $null) 
+if ($null -eq (Get-Command "scoop" -ErrorAction SilentlyContinue)) 
 { 
     $confirmation = Read-Host "No installation of scoop found. Install now? (y/n)"
     if ($confirmation -eq 'y') {
-        irm get.scoop.sh | iex
+        Invoke-RestMethod get.scoop.sh | Invoke-Expression
         scoop config aria2-enabled true
         scoop config aria2-warning-enabled false
     }
@@ -31,12 +31,12 @@ if ($confirmation -eq 'y') {
 
 $confirmation = Read-Host "Install extra applications and tools? (y/n)"
 if ($confirmation -eq 'y') {
-    echo "Installing voidtools.Everything..."
+    Write-Output "Installing voidtools.Everything..."
     winget install --id voidtools.Everything --source winget
-    echo "Installing PowerToys..."
+    Write-Output "Installing PowerToys..."
     winget install Microsoft.PowerToys --source winget
-    echo "Installing 7zip..."
+    Write-Output "Installing 7zip..."
     winget install --id 7zip.7zip --source winget
-    echo "Installing VSCode..."
+    Write-Output "Installing VSCode..."
     winget install --id Microsoft.VisualStudioCode --source winget
 }
