346447f9714162925d34c927c36057b1399bd25e
Joel T. Collins <joel.collins@ukkidney.org>
2023-11-06 14:25:40 +0000

    Add ~/Scripts to PowerShell path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -24,5 +24,8 @@ function Update-Profile {
 # Merge machine and user paths explicitally
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
+# Add local user Scripts directory to the path
+$Env:Path += ";$($env:USERPROFILE)\Scripts"
+
 # Invoke Starship prompt
 Invoke-Expression (&starship init powershell)
