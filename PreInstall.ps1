6be1ef34ba160ec3f6c82db2ae9a7df3c4079988
Joel T. Collins <joel.collins@prothea.tech>
2025-03-06 10:10:19 +0000

    chore: remove deprecated pscolor module

--- PreInstall.ps1
+++ PreInstall.ps1
@@ -3,6 +3,3 @@ winget install --id Git.Git --source winget
 winget install --id Microsoft.Powershell --source winget
 winget install --id Starship.Starship --source winget
 winget install --id Microsoft.WindowsTerminal --source winget
-
-# Install PowerShell modules
-Install-Module PSColor
